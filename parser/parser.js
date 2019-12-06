4e29d606bf5973453bef3dd852747702ac99a775
awphi <jellymoos3@gmail.com>
2019-12-06 19:15:07 +0000

    Parser

---
+++ parser/parser.js
@@ -0,0 +1,19 @@
+var fs = require('fs')
+const data = []
+
+
+fs.readFile('data.txt', 'utf8', function(err, contents) {
+	const txt = contents.toString().split("\n")
+
+	for (var i = txt.length - 1; i >= 0; i--) {
+		var a = txt[i]
+		data.push({
+			"description": a,
+			"penalty": 0
+		})
+	}
+
+	fs.writeFile('data.json', JSON.stringify(data), (err) => {
+		console.log('The file has been saved!');
+	});
+})
